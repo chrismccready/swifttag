2c8445cbde7892ceff58fb58780a79278bcf7d6e
Chris McCready <ccmcc@sonic.net>
2026-02-24 14:33:31 -0800

    Initial Commit

---
+++ SwiftTag/SwiftTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTagApp.swift
+//  SwiftTag
+//
+//  Created by Christopher McCready on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTagTests/SwiftTagTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTagTests.swift
+//  SwiftTagTests
+//
+//  Created by Christopher McCready on 2/24/26.
+//
+
+import Testing
+@testable import SwiftTag
+
+struct SwiftTagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
